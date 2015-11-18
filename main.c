012621cea8bc7c435ea95bcbdf0d29a548980e9c
AzariasB <azarias.boutin@gmail.com>
2015-11-18 14:34:35 +0000

    Work for some random values ... thanks Kevin !

--- main.c
+++ main.c
@@ -21,10 +21,11 @@
  * 
  */
 typedef struct material {
-    double mean_distance;
-    double scattering;
-    double absorbing;
-    float thickness;
+    //Free path : path a molecule before interact with one of the atom of the material
+    double mean_free_path;
+    //double scattering; // useless for the moment
+    double absorbing; //Absorbing component of the material
+    float thickness; //Thickness of the material
 } material;
 
 double r2()
@@ -42,22 +43,26 @@ void calcTransportation(int numberOfNeutron, material mat, int* res) {
         int bouncing = TRUE;
         while (bouncing) {
             double u = r2();
-            double distance = -(1 / mat.mean_distance) * log(u);
+            double distance = -(1 / mat.mean_free_path) * log(u);
             position += distance * cos(direction);
             printf("rand : %f, distance : %f,position : %f\n",u,distance,position);
             if (position < 0) { // Reflected
+                printf("Reflected\n");
                 reflected++;
                 bouncing = FALSE;
             } else if (position >= mat.thickness) { // Transmitted
+                printf("transmitted !\n");
                 transmitted++;
                 bouncing = FALSE;
-            } else if (u < mat.absorbing / mat.mean_distance) {//Absorbed
+            } else if (u < (mat.absorbing / mat.mean_free_path )) {//Absorbed
+                printf("Absorbed : beacuse %f less than : %f\n",u,(mat.absorbing/mat.mean_free_path));
                 absorbed++;
                 bouncing = FALSE;
             } else {
                 direction = u * M_PI;
             }
         }
+        printf("---end---\n");
     }
     res[0] = reflected;
     res[1] = absorbed;
@@ -65,11 +70,12 @@ void calcTransportation(int numberOfNeutron, material mat, int* res) {
 }
 
 int main(int argc, char** argv) {
+    time_t t;
+    srand((unsigned) time(&t));
     material mat;
-    mat.mean_distance = 0.25;
-    mat.absorbing = 2.56;
-    mat.scattering = 11.62;
-    mat.thickness = 10;
+    mat.mean_free_path = 1e-1; //Mean free path for ambiant pressure : 68 nm
+    mat.absorbing = 5e-2;
+    mat.thickness = 1; // In meters
     int answer[3];
     calcTransportation(10, mat, answer);
     printf("reflected : %d, absorbed : %d, transmitted : %d", answer[0], answer[1], answer[2]);
